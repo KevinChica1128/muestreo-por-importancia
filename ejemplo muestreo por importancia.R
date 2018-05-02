85db94a5427230885d5c0d117db27e4d2e0d632b
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-05-01 22:24:46 -0500

    parametros buenos

--- ejemplo muestreo por importancia.R
+++ ejemplo muestreo por importancia.R
@@ -48,29 +48,36 @@ y3=20
 y4=34
 a=b=1
 h<-function(x){((2+x)^y1)*((1-x)^(y2+y3+b-1))*x^(y4+a-1)}
+hexacta<-function(x){(1/2.357695e+28)*((2+x)^y1)*((1-x)^(y2+y3+b-1))*x^(y4+a-1)*(x^2)}
 #exacta
 integrate(h,0,1)
-Espteor<-integrate(h,0,1) #0.6228062
-Esp2teor<-integrate(h,0,1) #0.3904824
-Varteori<-0.3904824-(0.6228062)^2
+Espteor<-integrate(hexacta,0,1) #0.6228062
+Esp2teor<-integrate(hexacta,0,1) #0.3904824
+Varteori<-0.3904824-(0.6228062)^2 #0.002594837
 
 #Función de importancia Beta
-p<-rbeta(m,1,1)
-w<-h(p)/dbeta(p,1,1)
-E<-(1/sum(w))*(sum(w*p)) #0.6226815
-EB2<-(1/sum(w))*(sum(w*(p^2))) #0.3902821 #E(x^2)
-VB<-EB2-(E^2)
+p<-rbeta(m,54.723205,32.58249)
+w<-h(p)/dbeta(p,54.723205,32.58249)
+E<-(1/sum(w))*(sum(w*p)) #0.6230844
+EB2<-(1/sum(w))*(sum(w*(p^2))) #0.3908071 #E(x^2)
+VB<-EB2-(E^2) #0.00257293
 
 #Función de importancia normal
-p1<-rnorm(m,0.6268,0.002649)
-w1<-h(p1)/dnorm(p1,0.6268,0.002649)
-E1<-(1/sum(w1))*(sum(w1*p1)) #0.6255348
-EN2<-(1/sum(w1))*(sum(w1*(p1^2))) #0.3913354 #E(x^2)
-VN<-EN2-(E1^2) #0.000032416
+p1<-rnorm(m,0.6268,sqrt(0.002649))
+w1<-h(p1)/dnorm(p1,0.6268,sqrt(0.002649))
+E1<-(1/sum(w1))*(sum(w1*p1)) #0.623249
+EN2<-(1/sum(w1))*(sum(w1*(p1^2))) #0.3910104 #E(x^2)
+VN<-EN2-(E1^2) #0.0025704
 
 x11()
-plot(density(p),col="red",lty=2,lwd=2)
-lines(density(p1),lwd=2)
+plot(density(p),col="chartreuse4",ylab=expression(paste("h(",theta,"|y)"))
+     ,xlab = expression(theta),
+     main = "Aproximación distribuciones a posteriori",lty=5,lwd=2)
+curve(hexacta,0,1,10000,add=TRUE,lwd=3)
+lines(density(p1),lty=2,lwd=2,col="Red")
+legend("topright",legend=c("Exacta","Normal","Beta"),
+         col=c("Black","Red","chartreuse4"),lty=c(1,2,5))
+
 #----------------------------------------------------------#
 #N=20
 y11=14
@@ -78,23 +85,33 @@ y22=0
 y33=1
 y44=5
 h1<-function(x){((2+x)^y11)*((1-x)^(y22+y33+b-1))*x^(y44+a-1)}
+h1exacta<-function(x){(1/41575.13)*((2+x)^y11)*((1-x)^(y22+y33+b-1))*x^(y44+a-1)*(x^2)}
 #exacta
-integrate(h1,0,1)
-Espteor00<-integrate(h1,0,1) #0.8311239 
-Esp2teor00<-integrate(h1,0,1) #0.7024182
-Varteori00<-0.7024182-(0.8311239)^2
+integrate(h1,0,1) #Constante de integración
+Espteor00<-integrate(h1exacta,0,1) #0.8311239 
+Esp2teor00<-integrate(h1exacta,0,1) #0.7024182
+Varteori00<-0.7024182-(0.8311239)^2 #0.01165126
 
 #Función de importancia Beta
-p00<-rbeta(m,1,1)
-w00<-h1(p00)/dbeta(p00,1,1)
-E00<-(1/sum(w00))*(sum(w00*p00)) #0.8284109
-E200<-(1/sum(w00))*(sum(w00*p00^2)) #0.6981653
-V00<-E200-(E00^2)  #0.01190065
+p00<-rbeta(m,8.165764,0.873160)
+w00<-h1(p00)/dbeta(p00,8.165764,0.873160)
+E00<-(1/sum(w00))*(sum(w00*p00)) #0.83156
+E200<-(1/sum(w00))*(sum(w00*p00^2)) #0.7030455
+V00<-E200-(E00^2)  #0.01154752
 
-#Función de importancia normal
-p11<-rnorm(m,0.9034,0.008693)
-w11<-h1(p11)/dnorm(p11,0.9034,0.008693)
-E11<-(1/sum(w11))*(sum(w11*p11))  #0.9042977
-E211<-(1/sum(w11))*(sum(w11*p11^2)) #0.8181377
+#Función de importancia normal TRUNCARLA PENDIENTE
+p11<-rnorm(m,0.9034,sqrt(0.008693))
+w11<-h1(p11)/dnorm(p11,0.9034,sqrt(0.008693))
+E11<-(1/sum(w11))*(sum(w11*p11))  #1.224096
+E211<-(1/sum(w11))*(sum(w11*p11^2)) #1.494372
 V11<-E211-(E11^2) #0.0003833
+
+x11()
+plot(density(p00),col="chartreuse4",ylab=expression(paste("h(",theta,"|y)"))
+     ,xlab = expression(theta),
+     main = "Aproximación distribuciones a posteriori",lty=5,lwd=2)
+curve(h1exacta,0,1,10000,add=TRUE,lwd=3)
+lines(density(p11),lty=2,lwd=2,col="Red")
+legend("topright",legend=c("Exacta","Normal","Beta"),
+       col=c("Black","Red","chartreuse4"),lty=c(1,2,5))
 #-------------------------------------------------#
