27adaa53ba9d19163f1c0d2b62d9aae6d1525226
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-05-02 13:40:02 -0500

    presentacion ejemplos full menos el 3 xD

--- ejemplo muestreo por importancia.R
+++ ejemplo muestreo por importancia.R
@@ -10,32 +10,44 @@ E<-(1/sum(w))*(sum(w*theta1))  #Esperanza estimada
 V<-(1/sum(w))*(sum(w*(theta1^2))) #Varianza estimada
 #Gráfica comparativa:
 x11()
-plot(density(theta1),col="red",lty=2,lwd=2)
+plot(density(theta1),col="red",lty=2,lwd=2,ylab=expression(paste("h(",theta,"|y)"))
+     ,xlab = expression(theta),
+     main = "Aproximación distribuciones a posteriori")
 lines(density(X),lwd=2)
-
+legend("topright",legend=c("Exacta","T-Student"),
+       col=c("Black","Red"),lty=c(1,2))
 
 #Ejemplo 2:
-funcion<-function(x){(gamma(14)/(gamma(10)*gamma(4)))*(x^9)*((1-x)^3)*x} #E(X)
-funcion2<-function(x){(gamma(14)/(gamma(10)*gamma(4)))*(x^9)*((1-x)^3)*(x^2)} #E(X^2)
-Eteorica<-integrate(funcion,0,1)
-E2teorica<-integrate(funcion2,0,1)
+funcion<-function(x){(x^9)*((1-x)^3)}
+funcion2exacta<-function(x){(gamma(14)/(gamma(10)*gamma(4)))*(x^9)*((1-x)^3)*(x^2)} 
+integrate(funcion,0,1) #Constante de integración
+integrate(funcion2exacta,0,1) #Integral de la densidad completa (=1)
+Eteorica<-integrate(funcion2exacta,0,1)  #0.7142857
+E2teorica<-integrate(funcion2exacta,0,1) #0.5238095
 Vteorica<-0.5238095-(0.7142857^2) #Resultado: 0.01360544
-teta2<-runif(N)
-w1<-funcion(teta2)/dunif(teta2)
-E1<-(1/sum(w1))*(sum(w1*teta2))  #Esperanza estimada
-E21<-(1/sum(w1))*(sum(w1*(teta2^2))) #Varianza estimada
-V1<-E21-(E1^2) #0.01250114
 
-tetai2<-rbeta(N,1,1)
-w11<-funcion(tetai2)/dbeta(tetai2,1,1)
-E11<-(1/sum(w11))*(sum(w11*tetai2))  #Esperanza estimada
-E211<-(1/sum(w11))*(sum(w11*(tetai2^2))) #Varianza estimada
+teta2<-runif(N) #Funcion de importancia U(0,1)
+w1<-funcion(teta2)/dunif(teta2,0,1)
+E1<-(1/sum(w1))*(sum(w1*teta2))  #Esperanza estimada  0.7148448
+E21<-(1/sum(w1))*(sum(w1*(teta2^2))) #0.5247885
+V1<-E21-(E1^2) #Varianza estimada 0.01378547
 
-p<-runif(10000,0,1)
-A<-funcion(p)
-sum(A)/10000
-B<-funcion2(p)
-sum(B)/10000
+teta22<-rbeta(N,8.25,2.75) #Función de importancia Beta(8.25,2.75)
+w12<-funcion(teta22)/dbeta(teta22,8.25,2.75)
+E12<-(1/sum(w12))*(sum(w12*teta22))  #Esperanza estimada 0.7142896
+E212<-(1/sum(w12))*(sum(w12*(teta22^2))) #Varianza estimada 0.5238356
+V12<-E212-(E12^2) #0.01362596
+
+#Gráfica comparativa:
+x11()
+plot(density(teta22),col="chartreuse4",ylab=expression(paste("h(",theta,"|y)"))
+     ,xlab = expression(theta),
+     main = "Aproximación distribuciones a posteriori",lty=5,lwd=2)
+curve(funcion2exacta,0,1,10000,add=TRUE,lwd=3)
+lines(density(teta2),lty=2,lwd=2,col="Red")
+legend("topright",legend=c("Exacta","Uniforme","Beta"),
+       col=c("Black","Red","chartreuse4"),lty=c(1,2,5))
+
 
 #Ejemplo 3
 #----------------------------------------#
@@ -48,7 +60,7 @@ y3=20
 y4=34
 a=b=1
 h<-function(x){((2+x)^y1)*((1-x)^(y2+y3+b-1))*x^(y4+a-1)}
-hexacta<-function(x){(1/2.357695e+28)*((2+x)^y1)*((1-x)^(y2+y3+b-1))*x^(y4+a-1)*(x^2)}
+hexacta<-function(x){(1/2.357695e+28)*((2+x)^y1)*((1-x)^(y2+y3+b-1))*x^(y4+a-1)}
 #exacta
 integrate(h,0,1)
 Espteor<-integrate(hexacta,0,1) #0.6228062
@@ -85,7 +97,7 @@ y22=0
 y33=1
 y44=5
 h1<-function(x){((2+x)^y11)*((1-x)^(y22+y33+b-1))*x^(y44+a-1)}
-h1exacta<-function(x){(1/41575.13)*((2+x)^y11)*((1-x)^(y22+y33+b-1))*x^(y44+a-1)*(x^2)}
+h1exacta<-function(x){(1/41575.13)*((2+x)^y11)*((1-x)^(y22+y33+b-1))*x^(y44+a-1)}
 #exacta
 integrate(h1,0,1) #Constante de integración
 Espteor00<-integrate(h1exacta,0,1) #0.8311239 
